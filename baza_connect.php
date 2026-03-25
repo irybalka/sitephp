4d24c4979b5631cb46d75145947b4d0d16cabe46
Igor Rybalka <irybalka@yahoo.com>
2026-03-25 12:16:50 +0200

    Initial commit: add 7 site files

--- baza_connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$servername = "bezserv.mysql.tools"; //ім'я сервера бази даних
-	$username = "bezserv_baza1";// логін 
-	$password = "k+d-78L8gV";// пароль
-	$dbname = "bezserv_baza1";// ім'я бази даних (тільки тут співпадає з логіном)
